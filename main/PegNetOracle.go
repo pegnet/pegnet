6c26ddd603f0d01f801d67040b34ce09179adbf8
Paul Snow <snow.paul@gmail.com>
2019-05-27 16:43:26 -0500

    beginning of the conversion.  Still doesn't compile

--- main/PegNetOracle.go
+++ main/PegNetOracle.go
@@ -4,11 +4,33 @@ import (
 	"fmt"
 
 	"github.com/pegnet/OracleRecord"
+	"os/user"
+	"github.com/zpatrick/go-config"
 )
 
 func main() {
 	var opr oprecord.OraclePriceRecord
-	opr.GetOPRecord()
+
+	u, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	userPath := u.HomeDir
+	configfile := fmt.Sprintf("%s/.%s/miner%03d/config.ini", userPath, "pegnet", 1)
+	iniFile := config.NewINIFile(configfile)
+	Config := config.NewConfig([]config.Provider{iniFile})
+	_, err = Config.String("Miner.Protocol")
+	if err != nil {
+		configfile = fmt.Sprintf("%s/.%s/defaultconfig.ini", userPath, "pegnet")
+		iniFile := config.NewINIFile(configfile)
+		Config = config.NewConfig([]config.Provider{iniFile})
+		_, err = Config.String("Miner.Protocol")
+		if err != nil {
+			panic("Failed to open the config file for this miner, and couldn't load the default file either")
+		}
+	}
+
+	opr.GetOPRecord(Config)
 	fmt.Println(opr)
 }
 
--- opr.go
+++ opr.go
@@ -10,6 +10,7 @@ import (
 	"fmt"
 	"github.com/FactomProject/factom"
 	"github.com/zpatrick/go-config"
+	"encoding/json"
 )
 
 type OraclePriceRecord struct {
@@ -19,71 +20,55 @@ type OraclePriceRecord struct {
 	Nonce              [32]byte          // not part of OPR - nonce creacted by mining
 	EC                 *factom.ECAddress // not part of OPR - Entry Credit Address used by a miner
 	Entry              *factom.Entry     // not part of OPR - Entry to record this record
-	ChainID            [32]byte
-	Dbht               int32 // The Directory Block Height that this record is to contribute to.
-	VersionEntryHash   [32]byte
-	WinningPreviousOPR [32]byte
-	CoinbasePNTAddress [32]byte
-	BlockReward        [8]byte
-	FactomDigitalID    [32]byte
-	PNT                [8]byte
-	USD                [8]byte
-	EUR                [8]byte
-	JPY                [8]byte
-	GBP                [8]byte
-	CAD                [8]byte
-	CHF                [8]byte
-	INR                [8]byte
-	SGD                [8]byte
-	CNY                [8]byte
-	HKD                [8]byte
-	XAU                [8]byte
-	XAG                [8]byte
-	XPD                [8]byte
-	XPT                [8]byte
-	XBT                [8]byte
-	ETH                [8]byte
-	LTC                [8]byte
-	XBC                [8]byte
-	FCT                [8]byte
+	ChainID            [32]byte          `json:chainid`
+	Dbht               int32 			 `json:dbht` // The Directory Block Height that this record is to contribute to.
+	VersionEntryHash   [32]byte			 `json:version` // Entry hash for the PegNet version
+	WinningPreviousOPR [10][32]byte		 `json:winners` // Winning OPR entries in the previous block
+	CoinbasePNTAddress string			 `json:coinbase` // PNT Address to pay PNT
+	FactomDigitalID    [32]byte          `did` // Digital Identity of the miner
+	PNT                float64
+	USD                float64
+	EUR                float64
+	JPY                float64
+	GBP                float64
+	CAD                float64
+	CHF                float64
+	INR                float64
+	SGD                float64
+	CNY                float64
+	HKD                float64
+	XAU                float64
+	XAG                float64
+	XPD                float64
+	XPT                float64
+	XBT                float64
+	ETH                float64
+	LTC                float64
+	XBC                float64
+	FCT                float64
 }
 
-func (opr *OraclePriceRecord) GetTokens() (tokens []float64) {
-	add := func(v [8]byte) {
-		tokens = append(tokens, float64(binary.BigEndian.Uint64(v[:])))
-	}
-	add(opr.PNT)
-	add(opr.USD)
-	add(opr.EUR)
-	add(opr.JPY)
-	add(opr.GBP)
-	add(opr.CAD)
-	add(opr.CHF)
-	add(opr.INR)
-	add(opr.SGD)
-	add(opr.CNY)
-	add(opr.HKD)
-	add(opr.XAU)
-	add(opr.XAG)
-	add(opr.XPD)
-	add(opr.XPT)
-	add(opr.XBT)
-	add(opr.ETH)
-	add(opr.LTC)
-	add(opr.XBC)
-	add(opr.FCT)
-	return
+type Token struct {
+	code string
+	value float64
 }
 
-var lx *lxr.LXRHash
+var lx lxr.LXRHash
 
 func init() {
-	lx = new(lxr.LXRHash)
-	lx.Init(0x123412341234, 10240000, 256, 5)
+	lx.Init(0x123412341234, 32, 256, 5)
+}
+
+
+
+func (opr *OraclePriceRecord) GetTokens() []Token {
+	tokens := []Token{}
+	tokens = append(tokens, Token{"PNT", opr.PNT})
+	return tokens
 }
 
 func (opr *OraclePriceRecord) GetHash() []byte {
-	data, err := opr.MarshalBinary()
+	data, err := json.Marshal(opr)
 	check(err)
 	oprHash := lx.Hash(data)
 	return oprHash
@@ -135,131 +120,33 @@ func (opr *OraclePriceRecord) String() (str string) {
 	str = str + fmt.Sprintf("%32s %v\n", "Difficulty", opr.Difficulty)
 	str = str + fmt.Sprintf("%32s %v\n", "Directory Block Height", opr.Dbht)
 	print32("VersionEntryHash", opr.VersionEntryHash[:])
-	print32("WinningPreviousOPR", opr.WinningPreviousOPR[:])
-	print32("CoinbasePNTAddress", opr.CoinbasePNTAddress[:])
-	print32("BlockReward", opr.BlockReward[:])
-	print32("FactomDigitalID", opr.FactomDigitalID[:])
-	print32("PNT", opr.PNT[:])
-	print32("USD", opr.USD[:])
-	print32("EUR", opr.EUR[:])
-	print32("JPY", opr.JPY[:])
-	print32("GBP", opr.GBP[:])
-	print32("CAD", opr.CAD[:])
-	print32("CHF", opr.CHF[:])
-	print32("INR", opr.INR[:])
-	print32("SGD", opr.SGD[:])
-	print32("CNY", opr.CNY[:])
-	print32("HKD", opr.HKD[:])
-	print32("XAU", opr.XAU[:])
-	print32("XAG", opr.XAG[:])
-	print32("XPD", opr.XPD[:])
-	print32("XPT", opr.XPT[:])
-	print32("XBT", opr.XBT[:])
-	print32("ETH", opr.ETH[:])
-	print32("LTC", opr.LTC[:])
-	print32("XBC", opr.XBC[:])
-	print32("FCT", opr.FCT[:])
-	return str
-}
-
-func (opr *OraclePriceRecord) MarshalBinary() ([]byte, error) {
-	record := []byte{}
-
-	record = append(record, opr.ChainID[:]...)
-	record = append(record, byte(opr.Dbht>>24), byte(opr.Dbht>>16), byte(opr.Dbht>>8), byte(opr.Dbht))
-	record = append(record, opr.VersionEntryHash[:]...)
-	record = append(record, opr.WinningPreviousOPR[:]...)
-	record = append(record, opr.CoinbasePNTAddress[:]...)
-	record = append(record, opr.BlockReward[:]...)
-	record = append(record, opr.FactomDigitalID[:]...)
-	record = append(record, opr.PNT[:]...)
-	record = append(record, opr.USD[:]...)
-	record = append(record, opr.EUR[:]...)
-	record = append(record, opr.JPY[:]...)
-	record = append(record, opr.GBP[:]...)
-	record = append(record, opr.CAD[:]...)
-	record = append(record, opr.CHF[:]...)
-	record = append(record, opr.INR[:]...)
-	record = append(record, opr.SGD[:]...)
-	record = append(record, opr.CNY[:]...)
-	record = append(record, opr.HKD[:]...)
-	record = append(record, opr.XAU[:]...)
-	record = append(record, opr.XAG[:]...)
-	record = append(record, opr.XPD[:]...)
-	record = append(record, opr.XPT[:]...)
-	record = append(record, opr.XBT[:]...)
-	record = append(record, opr.ETH[:]...)
-	record = append(record, opr.LTC[:]...)
-	record = append(record, opr.XBC[:]...)
-	record = append(record, opr.FCT[:]...)
-	return record, nil
-}
-
-func (opr *OraclePriceRecord) UnmarshalBinary(data []byte) (err error) {
-	defer func() {
-		if r := recover(); r != nil {
-			err = errors.New("Data source does not have enough data")
-			return
-		}
-	}()
-	copy(opr.ChainID[:], data[:32])
-	data = data[32:]
-	opr.Dbht = int32(data[0])<<24 + int32(data[1])<<24 + int32(data[2])<<24 + int32(data[3])
-	data = data[4:]
-	copy(opr.VersionEntryHash[:], data[:32])
-	data = data[32:]
-	copy(opr.WinningPreviousOPR[:], data[:32])
-	data = data[32:]
-	copy(opr.CoinbasePNTAddress[:], data[:32])
-	data = data[32:]
-	copy(opr.BlockReward[:], data[:8])
-	data = data[8:]
-	copy(opr.FactomDigitalID[:], data[:32])
-	data = data[32:]
-	copy(opr.PNT[:], data[:8])
-	data = data[8:]
-	copy(opr.USD[:], data[:8])
-	data = data[8:]
-	copy(opr.EUR[:], data[:8])
-	data = data[8:]
-	copy(opr.JPY[:], data[:8])
-	data = data[8:]
-	copy(opr.GBP[:], data[:8])
-	data = data[8:]
-	copy(opr.CAD[:], data[:8])
-	data = data[8:]
-	copy(opr.CHF[:], data[:8])
-	data = data[8:]
-	copy(opr.INR[:], data[:8])
-	data = data[8:]
-	copy(opr.SGD[:], data[:8])
-	data = data[8:]
-	copy(opr.CNY[:], data[:8])
-	data = data[8:]
-	copy(opr.HKD[:], data[:8])
-	data = data[8:]
-	copy(opr.XAU[:], data[:8])
-	data = data[8:]
-	copy(opr.XAG[:], data[:8])
-	data = data[8:]
-	copy(opr.XPD[:], data[:8])
-	data = data[8:]
-	copy(opr.XPT[:], data[:8])
-	data = data[8:]
-	copy(opr.XBT[:], data[:8])
-	data = data[8:]
-	copy(opr.ETH[:], data[:8])
-	data = data[8:]
-	copy(opr.LTC[:], data[:8])
-	data = data[8:]
-	copy(opr.XBC[:], data[:8])
-	data = data[8:]
-	copy(opr.FCT[:], data[:8])
-	data = data[8:]
-	if len(data) > 0 {
-		err = errors.New("data source is too long for an OPR")
+	for _,v := range opr.WinningPreviousOPR {
+		print32("  WinningPreviousOPR", v[:])
 	}
-	return
+	str = str + fmt.Sprintf("%32s %v\n",opr.CoinbasePNTAddress)
+	print32("FactomDigitalID", opr.FactomDigitalID[:])
+
+	str = fmt.Sprintf("%s%32s %v\n",str,"PNT", opr.PNT)
+	str = fmt.Sprintf("%s%32s %v\n",str,"USD", opr.USD)
+	str = fmt.Sprintf("%s%32s %v\n",str,"EUR", opr.EUR)
+	str = fmt.Sprintf("%s%32s %v\n",str,"JPY", opr.JPY)
+	str = fmt.Sprintf("%s%32s %v\n",str,"GBP", opr.GBP)
+	str = fmt.Sprintf("%s%32s %v\n",str,"CAD", opr.CAD)
+	str = fmt.Sprintf("%s%32s %v\n",str,"CHF", opr.CHF)
+	str = fmt.Sprintf("%s%32s %v\n",str,"INR", opr.INR)
+	str = fmt.Sprintf("%s%32s %v\n",str,"SGD", opr.SGD)
+	str = fmt.Sprintf("%s%32s %v\n",str,"CNY", opr.CNY)
+	str = fmt.Sprintf("%s%32s %v\n",str,"HKD", opr.HKD)
+	str = fmt.Sprintf("%s%32s %v\n",str,"XAU", opr.XAU)
+	str = fmt.Sprintf("%s%32s %v\n",str,"XAG", opr.XAG)
+	str = fmt.Sprintf("%s%32s %v\n",str,"XPD", opr.XPD)
+	str = fmt.Sprintf("%s%32s %v\n",str,"XPT", opr.XPT)
+	str = fmt.Sprintf("%s%32s %v\n",str,"XBT", opr.XBT)
+	str = fmt.Sprintf("%s%32s %v\n",str,"ETH", opr.ETH)
+	str = fmt.Sprintf("%s%32s %v\n",str,"LTC", opr.LTC)
+	str = fmt.Sprintf("%s%32s %v\n",str,"XBC", opr.XBC)
+	str = fmt.Sprintf("%s%32s %v\n",str,"FCT", opr.FCT)
+	return str
 }
 
 func (opr *OraclePriceRecord) GetOPRecord(c *config.Config) {
@@ -289,12 +176,6 @@ func (opr *OraclePriceRecord) SetCoinbasePNTAddress(coinbaseAddress []byte) {
 	copy(opr.CoinbasePNTAddress[0:], coinbaseAddress)
 }
 
-func (opr *OraclePriceRecord) SetBlockReward(blockreward uint64) {
-	b := make([]byte, 8)
-	binary.BigEndian.PutUint64(b, blockreward)
-	copy(opr.BlockReward[0:], b[0:8])
-}
-
 func (opr *OraclePriceRecord) SetFactomDigitalID(factomDigitalID []byte) {
 	copy(opr.FactomDigitalID[0:], factomDigitalID)
 
@@ -303,46 +184,26 @@ func (opr *OraclePriceRecord) SetFactomDigitalID(factomDigitalID []byte) {
 func (opr *OraclePriceRecord) SetPegValues(assets PegAssets) {
 	b := make([]byte, 8)
 
-	binary.BigEndian.PutUint64(b, uint64(assets.PNT.Value))
-	copy(opr.PNT[0:], b[:])
-	binary.BigEndian.PutUint64(b, uint64(assets.USD.Value))
-	copy(opr.USD[0:], b[:])
-	binary.BigEndian.PutUint64(b, uint64(assets.EUR.Value))
-	copy(opr.EUR[0:], b[:])
-	binary.BigEndian.PutUint64(b, uint64(assets.JPY.Value))
-	copy(opr.JPY[0:], b[:])
-	binary.BigEndian.PutUint64(b, uint64(assets.GBP.Value))
-	copy(opr.GBP[0:], b[:])
-	binary.BigEndian.PutUint64(b, uint64(assets.CAD.Value))
-	copy(opr.CAD[0:], b[:])
-	binary.BigEndian.PutUint64(b, uint64(assets.CHF.Value))
-	copy(opr.CHF[0:], b[:])
-	binary.BigEndian.PutUint64(b, uint64(assets.INR.Value))
-	copy(opr.INR[0:], b[:])
-	binary.BigEndian.PutUint64(b, uint64(assets.SGD.Value))
-	copy(opr.SGD[0:], b[:])
-	binary.BigEndian.PutUint64(b, uint64(assets.CNY.Value))
-	copy(opr.CNY[0:], b[:])
-	binary.BigEndian.PutUint64(b, uint64(assets.HKD.Value))
-	copy(opr.HKD[0:], b[:])
-	binary.BigEndian.PutUint64(b, uint64(assets.XAU.Value))
-	copy(opr.XAU[0:], b[:])
-	binary.BigEndian.PutUint64(b, uint64(assets.XAG.Value))
-	copy(opr.XAG[0:], b[:])
-	binary.BigEndian.PutUint64(b, uint64(assets.XPD.Value))
-	copy(opr.XPD[0:], b[:])
-	binary.BigEndian.PutUint64(b, uint64(assets.XPT.Value))
-	copy(opr.XPT[0:], b[:])
-	binary.BigEndian.PutUint64(b, uint64(assets.XBT.Value))
-	copy(opr.XBT[0:], b[:])
-	binary.BigEndian.PutUint64(b, uint64(assets.ETH.Value))
-	copy(opr.ETH[0:], b[:])
-	binary.BigEndian.PutUint64(b, uint64(assets.LTC.Value))
-	copy(opr.LTC[0:], b[:])
-	binary.BigEndian.PutUint64(b, uint64(assets.XBC.Value))
-	copy(opr.XBC[0:], b[:])
-	binary.BigEndian.PutUint64(b, uint64(assets.FCT.Value))
-	copy(opr.FCT[0:], b[:])
+	opr.PNT = assets.PNT.Value
+	opr.USD =	assets.USD.Value
+	opr.EUR=assets.EUR.Value
+	opr.JPY=assets.JPY.Value
+	opr.GBP=assets.GBP.Value
+	opr.CAD=assets.CAD.Value
+	opr.CHF=assets.CHF.Value
+	opr.INR=assets.INR.Value
+	opr.SGD=assets.SGD.Value
+	opr.CNY=assets.CNY.Value
+	opr.HKD=assets.HKD.Value
+	opr.XAU=assets.XAU.Value
+	opr.XAG=assets.XAG.Value
+	opr.XPD=assets.XPD.Value
+	opr.XPT=assets.XPT.Value
+	opr.XBT=assets.XBT.Value
+	opr.ETH=assets.ETH.Value
+	opr.LTC=assets.LTC.Value
+	opr.XBC=assets.XBC.Value
+	opr.FCT=assets.FCT.Value
 
 }
 
@@ -353,7 +214,7 @@ func (opr *OraclePriceRecord) GetEntry(chainID string) *factom.Entry {
 	// An OPR record only has the nonce as an external ID
 	entryExtIDs := [][]byte{opr.Nonce[:]}
 	// The body Data is the marshal of the OPR
-	bodyData, err := opr.MarshalBinary()
+	bodyData, err := json.Marshal(opr)
 	check(err)
 	// Create the Entry struct
 	assetEntry := factom.Entry{ChainID: chainID, ExtIDs: entryExtIDs, Content: bodyData}
