c9065e6c2e18f7b06f9d282507b43640eca7d87f
Mitchell <17392435+MitchellBerry@users.noreply.github.com>
2019-07-20 10:47:27 +0800

    Adding / Fixing / Standardising Headers (#97)
    * added/moved/standardised license headers
    
    * typo
    
    * removed random indentation from config
    
    * indentation formatting

--- initialization/main.go
+++ initialization/main.go
@@ -1,7 +1,7 @@
-package main
-
 // Copyright (c) of parts are held by the various contributors (see the CLA)
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+package main
 
 import (
 	"encoding/hex"
--- polling/openexchangerates.go
+++ polling/openexchangerates.go
@@ -1,5 +1,6 @@
 // Copyright (c) of parts are held by the various contributors (see the CLA)
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
 package polling
 
 import (
