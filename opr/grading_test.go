a17afadc37cbb66ab21ea97f20521204d8dbacdf
Emyrk <stevenmasley@gmail.com>
2019-07-26 12:52:31 -0500

    Fix unit test

--- opr/grading_test.go
+++ opr/grading_test.go
@@ -141,7 +141,9 @@ func init() {
 	for i := 0; i < 100; i++ {
 		opr := NewOraclePriceRecord()
 		opr.Entry = new(factom.Entry)
-		opr.Entry.ExtIDs = [][]byte{{byte(i)}}
+		buf := make([]byte, 8)
+		binary.BigEndian.PutUint64(buf, opr.Difficulty)
+		opr.Entry.ExtIDs = [][]byte{{byte(i)}, buf}
 		opr.Entry.Content = []byte(fmt.Sprintf("Entry %05d Content for this entry", i))
 		opr.Difficulty = opr.ComputeDifficulty(opr.Entry.ExtIDs[0])
 		difficulty = append(difficulty, opr)
