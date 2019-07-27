5e1576f09dc0f5ea35403df12f2215392c932a05
Paul Snow <snow.paul@gmail.com>
2019-07-27 13:50:35 -0500

    initial work

---
+++ opr/fct2pFct.go
@@ -0,0 +1,36 @@
+// Copyright (c) of parts are held by the various contributors (see the CLA)
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+package opr
+
+import (
+	"github.com/FactomProject/factom"
+	"github.com/golangci/golangci-lint/pkg/config"
+)
+
+var FctDbht int64
+
+func UpdateBurns(config *config.Config) {
+	GetEntryBlocks(config)			// make sure we are up to date on Entry Blocks
+	if len(OPRBlocks) == 0 {
+		return // There is nothing to do if there is no OPR chain with valid OPR blocks
+	}
+	if FctDbht == 0 {
+		FctDbht = OPRBlocks[0].Dbht
+	}
+	for i:= FctDbht+1;;i++ {
+		fc,_,err := factom.GetFBlockByHeight(i)
+		if err != nil || fc == nil {
+			break
+		}
+		for _, tx := range fc.Transactions {
+			switch {
+			case len(tx.Inputs) != 1 ,
+			len(tx.Outputs)>0,
+			len(tx.ECOutputs) != 1,
+			tx.ECOutputs[0].Amount != 0,
+			tx.ECOutputs[0].Address !=
+
+			}
+		}
+	}
+}
