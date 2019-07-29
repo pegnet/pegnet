4c67acd2a1e43a0cceb1acb0fbb6c729e1568578
Who Soup <who.soup@gmail.com>
2019-07-29 15:05:42 +0200

    nonce increaser unit test

--- mining/miner_test.go
+++ mining/miner_test.go
@@ -1,7 +1,9 @@
 package mining_test
 
 import (
+	"bytes"
 	"crypto/sha256"
+	"encoding/hex"
 	"testing"
 
 	. "github.com/pegnet/pegnet/mining"
@@ -50,5 +52,49 @@ func testIncrement(b *testing.B) {
 	ni := NewNonceIncrementer(1)
 	for i := 0; i < b.N; i++ {
 		ni.NextNonce()
+	}
+}
+
+func TestNonceIncrementer(t *testing.T) {
+	incrs := make([]*NonceIncrementer, 0)
+	for i := 0; i < 256; i++ {
+		incrs = append(incrs, NewNonceIncrementer(i))
+	}
+	used := make(map[string]bool)
+
+	// convert []byte to int
+	c := func(b []byte) int {
+		var r int
+		for i := 0; i < len(b); i++ {
+			r <<= 8
+			r += int(b[i])
+		}
+		return r
+	}
+
+	var a int
+	for i := 0; i < 0x10000; i++ {
+		a = c(incrs[0].Nonce[1:])
+
+		if a != i {
+			t.Fatalf("n1 mismatched i. want = %d, got = %d, raw = %s", i, a, hex.EncodeToString(incrs[0].Nonce))
+		}
+
+		for _, inc := range incrs {
+			if bytes.Compare(incrs[0].Nonce[1:], inc.Nonce[1:]) != 0 {
+				t.Fatalf("mismatch at %d. n0 = %s, n%d = %s", i, hex.EncodeToString(incrs[0].Nonce[1:]), inc.Nonce[0], hex.EncodeToString(inc.Nonce[1:]))
+			}
+		}
+
+		for _, inc := range incrs {
+			if used[string(inc.Nonce)] {
+				t.Fatalf("nonce id%d %d already seen before", inc.Nonce[0], i)
+			}
+			used[string(inc.Nonce)] = true
+		}
+
+		for _, inc := range incrs {
+			inc.NextNonce()
+		}
 	}
 }
