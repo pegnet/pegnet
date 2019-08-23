fc063127f1f533a7f04d30dc44c16e08578b488e
Emyrk <stevenmasley@gmail.com>
2019-08-22 23:40:18 -0500

    price anaylsis csv

--- opr/recorder.go
+++ opr/recorder.go
@@ -31,6 +31,60 @@ func NewChainRecorder(con *config.Config, filpath string) (*ChainRecorder, error
 	return c, nil
 }
 
+func (c *ChainRecorder) WritePriceCSV(db database.IDatabase, height int64) error {
+	InitLX() // We intend to use the LX hash
+
+	f, err := os.OpenFile(c.filepath, os.O_CREATE|os.O_RDWR, 0666)
+	if err != nil {
+		return err
+	}
+	file := f
+	writer := csv.NewWriter(file)
+
+	b := balances.NewBalanceTracker()
+	g := NewQuickGrader(c.config, db, b)
+	var _ = g.Sync()
+
+	block := g.OprBlockByHeight(height)
+	if block == nil {
+		return fmt.Errorf("%d block is nil", height)
+	}
+	recLog.WithField("height", height).Infof("writing to csv")
+
+	err = writer.Write(append([]string{"ID"}, common.AllAssets...)) // Write headers
+	if err != nil {
+		return err
+	}
+
+	// Build the csv
+	for i, opr := range block.OPRs {
+		// fmt.Println(opr.FactomDigitalID)
+		line := []string{fmt.Sprintf("%d", i)}
+		for _, asset := range common.AllAssets {
+			// line = append(line, fmt.Sprintf("%d", int64(opr.Assets[asset]*1e4)%10000))
+			v := opr.Assets[asset]
+			// jitter
+			// sub := 1.0
+			// if rand.Intn(2) == 1 {
+			// 	sub = -1.0
+			// }
+			// jitter := (rand.Float64() / 1000) * sub
+			// v = v * jitter
+			// fmt.Println(jitter)
+			line = append(line, fmt.Sprintf("%.4f", v))
+		}
+
+		err = writer.Write(line)
+		if err != nil {
+			return err
+		}
+	}
+
+	writer.Flush()
+	var _ = file.Close()
+	return nil
+}
+
 // WriteMinerCSV will write all the miner related stats for a given chain to a csv. This includes
 // number of records, difficulties hit, and more.
 func (c *ChainRecorder) WriteMinerCSV() error {
