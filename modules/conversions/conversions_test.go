605ae19105cbff4357424964493ace9639109acf
sambarnes <mistersamuelbarnes@gmail.com>
2019-09-23 11:38:00 -0500

    Move vectored conversion tests to json

--- modules/conversions/conversions_test.go
+++ modules/conversions/conversions_test.go
@@ -1,8 +1,8 @@
 package conversions_test
 
 import (
+	"encoding/json"
 	"fmt"
-	"math"
 	"math/big"
 	"math/rand"
 	"testing"
@@ -24,103 +24,55 @@ import (
 // `maxError := max(X1/Y1, Y1/X1) + 1`
 // Where 1 is added to the result to account for truncation.
 type ConversionTest struct {
-	Name  string
-	Error string
-	XRate uint64
-	YRate uint64
-	X1    int64
-	Y1    int64
-	X2    int64
-	Y2    int64
+	Name        string `json:"name"`
+	ErrorString string `json:"error_string,omitempty"`
+	XRate       uint64 `json:"x_rate"`
+	YRate       uint64 `json:"y_rate"`
+	X1          int64  `json:"x1"`
+	Y1          int64  `json:"y1"`
+	X2          int64  `json:"x2,omitempty"`
+	Y2          int64  `json:"y2,omitempty"`
 }
 
-var conversionTestVectors = []ConversionTest{{
-	Name:  "invalid (zero fromRate)",
-	Error: "invalid rate: 0",
-	XRate: 0,
-	YRate: 1,
-	X1:    1,
-	Y1:    0,
-}, {
-	Name:  "invalid (zero toRate)",
-	Error: "invalid rate: 0",
-	XRate: 1,
-	YRate: 0,
-	X1:    1,
-	Y1:    0,
-}, {
-	Name:  "invalid (negative amount)",
-	Error: "invalid amount: must be greater than or equal to zero",
-	XRate: 1,
-	YRate: 1,
-	X1:    -1,
-	Y1:    0,
-}, {
-	Name:  "zero input",
-	XRate: 10000 * 1e8, // $10000 / input unit
-	YRate: 1e8,         // $1 / output unit
-	X1:    0,           // 0 input unit
-	Y1:    0,           // 0 output units
-}, {
-	Name:  "whole unit input",
-	XRate: 10000 * 1e8, // $10000 / input unit
-	YRate: 1e8,         // $1 / output unit
-	X1:    1e8,         // 1 input unit
-	Y1:    10000 * 1e8, // 10000 output units
-	X2:    1e8,
-	Y2:    10000 * 1e8,
-}, {
-	Name:  "half unit input",
-	XRate: 10000 * 1e8, // $10000 / input unit
-	YRate: 1e8,         // $1 / output unit
-	X1:    0.5e8,       // 1/2 input unit
-	Y1:    5000 * 1e8,  // 5000 output units
-	X2:    0.5e8,
-	Y2:    5000 * 1e8,
-}, {
-	Name:  "smallest unit input",
-	XRate: 10000 * 1e8, // $10000 / input unit
-	YRate: 1e8,         // $1 / output unit
-	X1:    1,           // 1e-8 input unit
-	Y1:    10000,       // 10000e-8 output units
-	X2:    1,
-	Y2:    10000,
-}, {
-	Name:  "smallest unit input (truncated result)",
-	XRate: 1e8,         // $1 / input unit
-	YRate: 10000 * 1e8, // $10000 / output unit
-	X1:    1,           // 1e-8 input unit
-	Y1:    0,           // 0 output units (due to truncation)
-	X2:    0,           // 0 (due to previous truncation)
-	Y2:    0,
-}}
+var conversionTestVectors = []string{
+	`{"name": "invalid (zero fromRate)", "error_string": "invalid rate: 0", "x_rate": 0, "y_rate": 1, "x1": 1, "y1": 0}`,
+	`{"name": "invalid (zero toRate)", "error_string": "invalid rate: 0", "x_rate": 1, "y_rate": 0, "x1": 1, "y1": 0}`,
+	`{"name": "invalid (negative amount)", "error_string": "invalid amount: must be greater than or equal to zero", "x_rate": 1, "y_rate": 1, "x1": -1, "y1": 0}`,
+	`{"name": "zero input", "x_rate": 1000000000000, "y_rate": 100000000, "x1": 0, "y1": 0}`,
+	`{"name": "whole unit input", "x_rate": 1000000000000, "y_rate": 100000000, "x1": 100000000, "y1": 1000000000000, "x2": 100000000, "y2": 1000000000000}`,
+	`{"name": "smallest unit input", "x_rate": 1000000000000, "y_rate": 100000000, "x1": 1, "y1": 10000, "x2": 1, "y2": 10000}`,
+	`{"name": "smallest unit input (truncated result)", "x_rate": 100000000, "y_rate": 1000000000000, "x1": 1, "y1": 0, "x2": 0, "y2": 0}`,
+}
 
 func TestConversions_Convert_Vectors(t *testing.T) {
-	for _, test := range conversionTestVectors {
+	for _, testJSON := range conversionTestVectors {
+		var test ConversionTest
+		err := json.Unmarshal([]byte(testJSON), &test)
+		require.NoError(t, err, "ConversionTest JSON Unmarshal raised an unexpected error")
 		t.Run(test.Name, func(t *testing.T) {
 			assert := assert.New(t)
 
 			observedY1, err := Convert(test.X1, test.XRate, test.YRate)
-			if len(test.Error) != 0 {
-				assert.EqualError(err, test.Error)
+			if len(test.ErrorString) != 0 {
+				assert.EqualError(err, test.ErrorString)
 				return
 			}
 			require.NoError(t, err)
-			assert.Equal(test.Y1, observedY1, "Unexpected result for conversion: X1 --> Y1")
+			require.Equal(t, test.Y1, observedY1, "Unexpected result for conversion: X1 --> Y1")
 
 			// Due to truncation in integer division, there is often error present in the
 			// conversion from Y back to X. Thus, we check that it is within the expected
 			// margin of error.
 			observedX2, err := Convert(test.Y1, test.YRate, test.XRate)
 			require.NoError(t, err)
-			observedError := int64(math.Abs(float64(test.X1 - observedX2)))
+			observedError := abs(test.X1 - observedX2)
 			maxExpectedError := maxConversionError(test.X1, test.Y1)
-			assert.True(observedError <= maxExpectedError, "Margin of error exceeded for conversion Y1 --> X2")
+			require.True(t, observedError <= maxExpectedError, "Margin of error exceeded for conversion Y1 --> X2")
 
 			observedY2, err := Convert(test.X2, test.XRate, test.YRate)
 			require.NoError(t, err)
-			assert.Equal(test.Y2, observedY2, "Unexpected result for conversion: X2 --> Y2")
-			assert.Equal(observedY1, observedY2, "Y1 != Y2")
+			observedError = abs(test.Y1 - observedY2)
+			assert.True(observedError <= maxExpectedError, "Margin of error exceeded for conversion X2 --> Y2")
 		})
 	}
 }
@@ -149,13 +101,13 @@ func TestConversions_Convert_Random(t *testing.T) {
 			// margin of error.
 			x2, err := Convert(y1, yRate, xRate)
 			require.NoError(t, err)
-			observedError := int64(math.Abs(float64(x1 - x2)))
+			observedError := abs(x1 - x2)
 			maxExpectedError := maxConversionError(x1, y1)
 			assert.True(observedError <= maxExpectedError, "Margin of error exceeded for conversion Y1 --> X2")
 
 			y2, err := Convert(x2, xRate, yRate)
 			require.NoError(t, err)
-			observedError = int64(math.Abs(float64(y1 - y2)))
+			observedError = abs(y1 - y2)
 			assert.True(observedError <= maxExpectedError, "Margin of error exceeded for conversion X2 --> Y2")
 		})
 	}
@@ -174,4 +126,11 @@ func maxConversionError(x, y int64) int64 {
 		return ratioYX.Int64() + 1
 	}
 	return ratioXY.Int64() + 1
+}
+
+func abs(x int64) int64 {
+	if x < 0 {
+		return 0 - x
+	}
+	return x
 }
