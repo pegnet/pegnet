0e2ab355fbfb14de52cf27af61441ae0500e16f3
Emyrk <stevenmasley@gmail.com>
2019-09-23 09:27:41 -0500

    Added two simple tests to ensure the fixed prices are correct

--- polling/fixedpeg_test.go
+++ polling/fixedpeg_test.go
@@ -2,6 +2,8 @@ package polling_test
 
 import (
 	"testing"
+
+	"github.com/pegnet/pegnet/polling"
 )
 
 func TestFixedPEGPeggedAssets(t *testing.T) {
@@ -10,4 +12,16 @@ func TestFixedPEGPeggedAssets(t *testing.T) {
 
 func TestActualFixedPEGPeggedAssets(t *testing.T) {
 	ActualDataSourceTest(t, "FixedPEG")
+}
+
+// Ensure price is fixed at 0
+func TestNewFixedPEGDataSource(t *testing.T) {
+	d, _ := polling.NewDataSource("FixedPEG", nil)
+	v, err := d.FetchPegPrice("PEG")
+	if err != nil {
+		t.Error(err)
+	}
+	if v.Value != 0 {
+		t.Error("Exp a 0 for the value of PEG")
+	}
 }
