e95ee3ab219a75477ab16dcd6081579f4203ef68
dragonheaven <happyfamilylife11@gmail.com>
2020-09-15 21:43:06 +0300

    s3 grade algorithm

--- modules/graderStake/s3grader.go
+++ modules/graderStake/s3grader.go
@@ -1,31 +1,31 @@
 package graderStake
 
-var _ BlockGrader = (*S2BlockGrader)(nil)
+var _ BlockGrader = (*S3BlockGrader)(nil)
 
-// S2BlockGrader implements the s2 grading algorithm.
+// S3BlockGrader implements the S3 grading algorithm.
 // Entries are encoded in Protobuf with 25 winners each block.
 // Valid assets can be found in ´opr.V5Assets´
-type S2BlockGrader struct {
+type S3BlockGrader struct {
 	baseGrader
 }
 
 // Version 6
-func (s2 *S2BlockGrader) Version() uint8 {
+func (s3 *S3BlockGrader) Version() uint8 {
 	return 6
 }
 
 // WinnerAmount is the number of SPRs that receive a payout
-func (s2 *S2BlockGrader) WinnerAmount() int {
+func (s3 *S3BlockGrader) WinnerAmount() int {
 	return 25
 }
 
-// AddSPR verifies and adds a s2 SPR.
-func (s2 *S2BlockGrader) AddSPR(entryhash []byte, extids [][]byte, content []byte) error {
-	gspr, err := ValidateS2(entryhash, extids, s2.height, content)
+// AddSPR verifies and adds a s3 SPR.
+func (s3 *S3BlockGrader) AddSPR(entryhash []byte, extids [][]byte, content []byte) error {
+	gspr, err := ValidateS3(entryhash, extids, s3.height, content)
 	if err != nil {
 		return err
 	}
-	s2.sprs = append(s2.sprs, gspr)
+	s3.sprs = append(s3.sprs, gspr)
 	return nil
 }
 
@@ -40,22 +40,22 @@ func (s2 *S2BlockGrader) AddSPR(entryhash []byte, extids [][]byte, content []byt
 //	6. Repeat 3-4 until there are only 25 SPRs left
 //	7. Repeat 3 but this time don't apply the band and don't throw out SPRs, just reorder them
 //	until you are left with one
-func (s2 *S2BlockGrader) Grade() GradedBlock {
-	return s2.GradeCustom(50)
+func (s3 *S3BlockGrader) Grade() GradedBlock {
+	return s3.GradeCustom(50)
 }
 
 // GradeCustom grades the block using a custom cutoff for the top X
-func (s2 *S2BlockGrader) GradeCustom(cutoff int) GradedBlock {
-	block := new(S2GradedBlock)
+func (s3 *S3BlockGrader) GradeCustom(cutoff int) GradedBlock {
+	block := new(S3GradedBlock)
 	block.cutoff = cutoff
-	block.height = s2.height
-	block.cloneSPRS(s2.sprs)
+	block.height = s3.height
+	block.cloneSPRS(s3.sprs)
 	block.filterDuplicates()
 	block.grade()
 	return block
 }
 
 // Payout returns the amount of Pegtoshi awarded to the SPR at the specified index
-func (s2 *S2BlockGrader) Payout(index int) int64 {
+func (s3 *S3BlockGrader) Payout(index int) int64 {
 	return S1Payout(index)
 }
