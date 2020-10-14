432a4595530079b63b7ff3ef95e9607696ac7862
dragonheaven <happyfamilylife11@gmail.com>
2020-10-14 17:16:13 +0300

    test environment

--- common/common.go
+++ common/common.go
@@ -50,7 +50,7 @@ const (
 
 const (
 	MainNetwork     = "MainNet"
-	TestNetwork     = "TestNet-pM2"
+	TestNetwork     = "TestNet-pM4"
 	UnitTestNetwork = "unit-test" // Only used for unit tests
 
 	MainNetworkRCD = MainNetwork + "RCD"
