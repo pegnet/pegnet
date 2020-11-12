1c3e624589a5853e2a7c6df81a42dab0214e8cca
StarNeit <koneits@outlook.com>
2020-11-12 18:30:00 +0100

    testnetwork network

--- common/common.go
+++ common/common.go
@@ -50,7 +50,7 @@ const (
 
 const (
 	MainNetwork     = "MainNet"
-	TestNetwork     = "TestNet-pM3"
+	TestNetwork     = "TestNet-pM7"
 	UnitTestNetwork = "unit-test" // Only used for unit tests
 
 	MainNetworkRCD = MainNetwork + "RCD"
